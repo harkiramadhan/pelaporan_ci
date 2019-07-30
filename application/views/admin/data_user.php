03059b7c7ef08366af341a8361182af7e0168657
harkiramadhan <harkiramadhan@gmail.com>
2019-07-30 18:35:53 +0700

    user tab walisantri

--- application/views/admin/data_user.php
+++ application/views/admin/data_user.php
@@ -1,76 +1,159 @@
-<!-- Header -->
-<div class="header bg-gradient-default pb-8 pt-3 pt-md-5">
-</div>
+    <!-- Header -->
+    <div class="header bg-gradient-default pb-8 pt-3 pt-md-5">
+    <div class="container-fluid">
+        <div class="nav-wrapper">
+          <ul class="nav nav-pills flex-row flex-md-row" id="tabs-icons-text" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link mb-sm-3 mb-md-2 active" id="tabs-icons-text-1-tab" data-toggle="tab" href="#musyrif" role="tab" aria-controls="tabs-icons-text-1" aria-selected="true">Admin / Musyrif</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link mb-sm-3 mb-md-2 ml-3 ml-sm-0" id="tabs-icons-text-2-tab" data-toggle="tab" href="#walisantri" role="tab" aria-controls="tabs-icons-text-2" aria-selected="false">Walisantri</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
     <!-- Page content -->
     <div class="container-fluid mt--8">
     <!-- Table -->
     <div class="row">
         <div class="col">
             <div class="card shadow">
-                <div class="card-header bg-white border-0">
-                    <div class="row align-items-center">
-                        <div class="col">
-                            <h3 class="mb-0">Data User</h3>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="musyrif" role="tabpanel" aria-labelledby="tabs-icons-text-1-tab">
+                        <div class="card-header bg-white border-0">
+                            <div class="row align-items-center">
+                                <div class="col">
+                                    <h3 class="mb-0">Data User Musyrif / Admin</h3>
+                                </div>
+                                <div class="col-3 text-right">
+                                    <input type="text" id="myInput" placeholder="Cari User ..." class="form-control form-control-alternative form-control-sm">
+                                </div>
+                                <div class="col-2">
+                                    <button type="button" data-toggle="modal" data-target="#tambah" class="btn btn-info">Tambah User</button>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-3 text-right">
-                            <input type="text" id="myInput" placeholder="Cari User ..." class="form-control form-control-alternative form-control-sm">
-                        </div>
-                        <div class="col-2">
-                            <button type="button" data-toggle="modal" data-target="#tambah" class="btn btn-info">Tambah User</button>
+                        <div class="table-responsive">
+                            <table class="table align-items-center table-flush">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th scope="col" width="10">No</th>
+                                        <th scope="col">Username</th>
+                                        <th scope="col">Nama</th>
+                                        <th scope="col">Status</th>
+                                        <th scope="col">Role</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                    $no = 1;
+                                    foreach($user as $row){
+                                    ?>
+                                    <tr>
+                                        <td scope="row">1</td>
+                                        <td scope="row">
+                                            <div class="media align-items-center">
+                                                <div class="media-body">
+                                                    <span class="mb-0 text-sm"><strong><?= $row->username ?></strong></span>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td scope="row"><strong><?= $row->nama ?></strong></td>
+                                        <td>
+                                            <span class="badge badge-dot mr-4">
+                                                <?php if($row->status == 'aktif' || $row->status == 'Aktif'): ?>
+                                                    <i class="bg-success"></i>Aktif</span>
+                                                <?php else: ?>
+                                                    <i class="bg-warning"></i>Non Aktif</span>
+                                                <?php endif; ?></td>
+                                            </span>
+                                        </td>
+                                        <td>
+                                            <?php if($row->role == '1'): ?>
+                                                Admin
+                                            <?php else: ?>
+                                                Musyrif
+                                            <?php endif; ?>
+                                        </td>
+                                        <td>
+                                            <button type="button" data-toggle="modal" data-target="#edit<?= $row->id ?>" class="btn btn-sm btn-secondary">Edit</button>
+                                            <button type="button" data-toggle="modal" data-target="#delete<?= $row->id ?>" class="btn btn-sm btn-danger">Hapus</button>
+                                        </td>
+                                    </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-                </div>
-                <div class="table-responsive">
-                    <table class="table align-items-center table-flush">
-                        <thead class="thead-light">
-                            <tr>
-                                <th scope="col" width="10">No</th>
-                                <th scope="col">Username</th>
-                                <th scope="col">Nama</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Role</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $no = 1;
-                            foreach($user as $row){
-                            ?>
-                            <tr>
-                                <td scope="row">1</td>
-                                <td scope="row">
-                                    <div class="media align-items-center">
-                                        <div class="media-body">
-                                            <span class="mb-0 text-sm"><strong><?= $row->username ?></strong></span>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td scope="row"><strong><?= $row->nama ?></strong></td>
-                                <td>
-                                    <span class="badge badge-dot mr-4">
-                                        <?php if($row->status == 'aktif' || $row->status == 'Aktif'): ?>
-                                            <i class="bg-success"></i>Aktif</span>
-                                        <?php else: ?>
-                                            <i class="bg-warning"></i>Non Aktif</span>
-                                        <?php endif; ?></td>
-                                    </span>
-                                </td>
-                                <td>
-                                    <?php if($row->role == '1'): ?>
-                                        Admin
-                                    <?php else: ?>
-                                        Musyrif
-                                    <?php endif; ?>
-                                </td>
-                                <td>
-                                    <button type="button" data-toggle="modal" data-target="#edit<?= $row->id ?>" class="btn btn-sm btn-secondary">Edit</button>
-                                    <button type="button" data-toggle="modal" data-target="#delete<?= $row->id ?>" class="btn btn-sm btn-danger">Hapus</button>
-                                </td>
-                            </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                    <div class="tab-pane fade" id="walisantri" role="tabpanel" aria-labelledby="tabs-icons-text-2-tab">
+                        <div class="card-header bg-white border-0">
+                            <div class="row align-items-center">
+                                <div class="col">
+                                    <h3 class="mb-0">Data User Musyrif / Admin</h3>
+                                </div>
+                                <div class="col-3 text-right">
+                                    <input type="text" id="myInput" placeholder="Cari User ..." class="form-control form-control-alternative form-control-sm">
+                                </div>
+                                <div class="col-2">
+                                    <button type="button" data-toggle="modal" data-target="#tambah" class="btn btn-info">Tambah User</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table align-items-center table-flush">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th scope="col" width="10">No</th>
+                                        <th scope="col">Username</th>
+                                        <th scope="col">Nama</th>
+                                        <th scope="col">Status</th>
+                                        <th scope="col">Role</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                    $no = 1;
+                                    foreach($user as $row){
+                                    ?>
+                                    <tr>
+                                        <td scope="row">1</td>
+                                        <td scope="row">
+                                            <div class="media align-items-center">
+                                                <div class="media-body">
+                                                    <span class="mb-0 text-sm"><strong><?= $row->username ?></strong></span>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td scope="row"><strong><?= $row->nama ?></strong></td>
+                                        <td>
+                                            <span class="badge badge-dot mr-4">
+                                                <?php if($row->status == 'aktif' || $row->status == 'Aktif'): ?>
+                                                    <i class="bg-success"></i>Aktif</span>
+                                                <?php else: ?>
+                                                    <i class="bg-warning"></i>Non Aktif</span>
+                                                <?php endif; ?></td>
+                                            </span>
+                                        </td>
+                                        <td>
+                                            <?php if($row->role == '1'): ?>
+                                                Admin
+                                            <?php else: ?>
+                                                Musyrif
+                                            <?php endif; ?>
+                                        </td>
+                                        <td>
+                                            <button type="button" data-toggle="modal" data-target="#edit<?= $row->id ?>" class="btn btn-sm btn-secondary">Edit</button>
+                                            <button type="button" data-toggle="modal" data-target="#delete<?= $row->id ?>" class="btn btn-sm btn-danger">Hapus</button>
+                                        </td>
+                                    </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
